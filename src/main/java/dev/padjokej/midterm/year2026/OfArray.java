0398e7080b60f2d3a7cdc8a4241670995a9bda57
PadjokeJ <jonatan@padjokej.dev>
2026-04-17 15:07:49 +0200

    fix: clone array

--- src/main/java/dev/padjokej/midterm/year2026/OfArray.java
+++ src/main/java/dev/padjokej/midterm/year2026/OfArray.java
@@ -4,7 +4,7 @@ public final class OfArray<E> extends AbstractImmuList<E> {
     private final E[] array;
 
     public OfArray (E[] arr) {
-        array = arr;
+        array = arr.clone();
     }
 
     @Override
